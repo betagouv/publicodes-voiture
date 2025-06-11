b45911dd5849f3872a0ef6e43c220ef9ab15895a
Emile Rolley <emile.rolley@tuta.io>
2025-06-11 15:58:03 +0200

    fix(app): correctly setup netlify deploy

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from "@sveltejs/adapter-auto"
+import adapter from "@sveltejs/adapter-netlify"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 import { mdsvex } from "mdsvex"
 
 const config = {
   preprocess: [vitePreprocess(), mdsvex()],
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter({
+      split: true,
+    }),
+  },
   extensions: [".svelte", ".svx"],
 }
 
